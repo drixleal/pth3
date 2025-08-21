834c51f43c1e83c34a5e7be800e10d2923bb3fac
Drix Leal <drix.leal95@gmail.com>
2025-08-21 20:04:17 -0300

    add Java code

---
+++ CODE/test_3/Hello.java
@@ -0,0 +1,13 @@
+/**
+  Hello World Java Program
+  @author Drix.Leal
+ */
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!\n");
+        System.out.println("This is a simple Java program.\n");
+    }
+}
