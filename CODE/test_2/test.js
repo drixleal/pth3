c9a99f7c9fc7bf7bd2aa508c1bdbe378d544ab37
Drix Leal <drix.leal95@gmail.com>
2025-08-21 14:40:48 -0300

    update JS

--- CODE/test_2/test.js
+++ CODE/test_2/test.js
@@ -1,1 +1,16 @@
-console.log ("This is test 2");
+//Variáveis Express e App
+var express = require ('express'); 
+var app = express();
+
+//Texto no browser
+app.get('/', function (req, res) 
+{
+  res.send('Filho de uma vagabunda!! PERDÃO!!');
+});
+
+//Servidor rodando na porta 9536
+//Para acessar: http://localhost:9536/ ou http://192.168.10.5:9536/
+app.listen(9536, function()
+{
+    console.log('Servidor rodando na porta 9536');
+});
