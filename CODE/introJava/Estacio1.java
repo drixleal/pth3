2d28998286c30d96129bce9364e47b6e1d712e41
Drix Leal <drix.leal95@gmail.com>
2025-09-18 10:55:36 -0300

    est1

--- CODE/introJava/Estacio1.java
+++ CODE/introJava/Estacio1.java
@@ -1,18 +1,34 @@
+
+import java.util.Random;
+
 public class Estacio1
 {
-public abstract strict class Estacio1 
+
     
-        extends Pai implements Idealizacao, Sonho
-        {
-            private String nome;
-            public void inserirNome(String nn)
-            {
-                nome = nn;
-            }
-            public String retornarNome()
-            {
-                return nome;
-            }
-        }    
-    
+    //Atributos
+    private String nome;
+    private double codigo_identificador;
+    private Random aleatorio;   
+    //Métodos
+    public Estacio1 (String nome){
+         aleatorio = new Random();
+         this.setNome(nome);
+         this.codigo_identificador = aleatorio.nextDouble();
+    }
+    private void setNome (String nome) {
+         this.nome = nome;
+    }  
+    public String getNome () {
+         return this.nome;
+    }
+    public double getCodigoIdentificador (){
+         return this.codigo_identificador;
+    }   
+    public static void main(String args[]){
+        Estacio1 p1 = new Estacio1("Teste A");
+        System.out.println("Pessoa 1: "+p1.getNome());
+        Estacio1 p2 = new Estacio1("Teste B");
+        System.out.println("[Pessoa 1] nome: "+p1.getNome()+", Codigo Identificador: "+p1.getCodigoIdentificador());
+        System.out.println("[Pessoa 2] nome: "+p2.getNome()+", Codigo Identificador: "+p2.getCodigoIdentificador());
+    }
 }
