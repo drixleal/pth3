0d191adec9363b5e38a32084478127df2c5f8555
Drix Leal <drix.leal95@gmail.com>
2025-09-18 09:46:31 -0300

    Estacio1

---
+++ CODE/introJava/Estacio1.java
@@ -0,0 +1,18 @@
+public class Estacio1
+{
+public abstract strict class Estacio1 
+    
+        extends Pai implements Idealizacao, Sonho
+        {
+            private String nome;
+            public void inserirNome(String nn)
+            {
+                nome = nn;
+            }
+            public String retornarNome()
+            {
+                return nome;
+            }
+        }    
+    
+}
