20373a32fc38c527d321ff5cf5ec847e2c06bb2c
Drix Leal <drix.leal95@gmail.com>
2025-11-11 16:14:06 -0300

    html e js

---
+++ site-php/testes/script.js
@@ -0,0 +1,9 @@
+const imc = (peso, altura) => peso / (altura * altura);
+const situacao = (valor_imc) => {
+  if (valor_imc < 18.5) return "Abaixo do peso";
+  else if (valor_imc < 25) return "Peso ideal (parabéns)";
+  else if (valor_imc < 30) return "Levemente acima do peso";
+  else if (valor_imc < 35) return "Obesidade grau I";
+  else if (valor_imc < 40) return "Obesidade grau II";
+  else return "Obesidade grau III";
+};
